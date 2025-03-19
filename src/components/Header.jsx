90bd5cd723de4e7f93c1f49316de80da80fb3ac8
SrinidhiKothakapu <srinidhikothakapu@gmail.com>
2025-03-19 10:21:19 +0530

    added

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import "./Header.css"
+export default function Header(){
+    return (
+        <div className="App-Header-Row">
+            <div>React store</div>
+            <div>menu</div>
+        </div>
+
+    )
+}
